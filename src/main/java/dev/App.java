b5519137166059510838f144ad1dd7e3d0ecea45
LAAGUIDI <laaguidi.cherif90@gmail.com>
2019-02-12 12:07:58 +0100

    la fin du tp maven 1

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,5 @@
 package dev;
-
+import com.github.lalyos.jfiglet.FigletFont;
 /**
  * Hello world!
  *
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String asciiArt = FigletFont.convertOneLine("Hello World!");
+        System.out.println(asciiArt);
     }
 }
+
+
+
+
+
